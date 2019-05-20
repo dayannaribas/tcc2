3c4821099233216ba0baefd6c652f6696310a459
Daiana Ribas <d.ayann@hotmail.com>
2019-05-19 23:09:14 -0300

    Registrando gerenciamento das mensagens para a página de admin.

--- cadastro/admin.py
+++ cadastro/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Cadastro
+from .models import Cadastro, Mensagem
 
 admin.site.register(Cadastro)
+admin.site.register(Mensagem)
